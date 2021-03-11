7b9b825672c93c558ca045f16a243c697f3fc988
Mina Nabil <mina9492@hotmail.com>
2021-03-11 08:28:17 +0200

    Adding Ahmed and Aly accounts

--- MotorCity/database/seeders/UserSeeder.php
+++ MotorCity/database/seeders/UserSeeder.php
@@ -14,18 +14,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // DB::table('users')->insert([
+        //     'name' => "Adel Mahmoud",
+        //     'email' => 'adelmahmoud1997@gmail.com',
+        //     'password' => Hash::make('password'),
+        //     'admin' => true,
+        //     'brandId'=> -1
+        // ]);
+        // DB::table('users')->insert([
+        //     'name' => "Admin",
+        //     'email' => 'admin',
+        //     'password' => Hash::make('baba@motorcity'),
+        //     'admin' => true,
+        //     'brandId'=> 1
+        // ]);
         DB::table('users')->insert([
-            'name' => "Adel Mahmoud",
-            'email' => 'adelmahmoud1997@gmail.com',
-            'password' => Hash::make('password'),
+            'name' => "Ahmed Ibrahem",
+            'email' => 'Ahmed Ibrahem',
+            'password' => Hash::make('1984'),
             'admin' => true,
-            'brandId'=> -1
+            'brandId'=> 1
         ]);
         DB::table('users')->insert([
-            'name' => "Admin",
-            'email' => 'admin',
-            'password' => Hash::make('baba@motorcity'),
-            'admin' => false,
+            'name' => "Aly",
+            'email' => 'Aly',
+            'password' => Hash::make('aly@motorcity'),
+            'admin' => true,
             'brandId'=> 1
         ]);
     }
