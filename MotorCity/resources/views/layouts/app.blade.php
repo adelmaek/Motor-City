ddb0658fff1f56172d0cbcb24de00a5afe3d9f17
Mina Nabil <mina9492@hotmail.com>
2021-03-13 12:54:44 +0200

    Touches 3nd el client

--- MotorCity/resources/views/layouts/app.blade.php
+++ MotorCity/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Motor City</title>
+    <title>Motorcity</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
